8905e1fa522a484a6363feee40785bab62769999
nikla <nikla@EJPATOLITEJ>
2021-09-30 18:32:42 +0200

    Add project files.

---
+++ Individuellt Projekt Banksimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Individuellt_Projekt_Banksimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
